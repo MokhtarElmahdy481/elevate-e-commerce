7bf3b8e59efbc32dd3d62bf299b4ca1cbb1c9f0b
MokhtarElmahdy481 <mokhtarmahdy481@gmail.com>
2024-10-19 10:53:14 +0300

    Merge branch 'dev' of https://github.com/MokhtarElmahdy481/elevate-e-commerce into feat/create-products

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-import { useEffect, useState } from 'react'
-import ProductList from "./components/product/productList"
-import { useGetProducts } from './hooks';
+import './App.css'
+import RoutesProvider from './utils/router'
 
 function App() {
-  const {
-    products,
-    loading,
-    error,
-} = useGetProducts()
-  // useEffect(()=>{
-  //   fetch('https://fakestoreapi.com/products')
-  //   .then(res=>res.json())
-  //   .then(data=>setData(data))
-  //   .catch(error => {
-  //     console.log(error);
-      
-  //   })
-  // },[])
+ 
 
-  // useEffect(()=>{
-  //   fetch('https://fakestoreapi.com/products')
-  //   .then(res=>res.json())
-  //   .then(data=>setData(data))
-  //   .catch(error => {
-  //     console.log(error);
-      
-  //   })
-  // },[])
-  console.log("data",products);
-  
   return (
-    <div className='container'>
-      {/* <div className=""> */}
-
-      <ProductList products={products} />
-      {/* </div> */}
-        
-    </div>
+    <>
+      <RoutesProvider/>
+    </>
   )
 }
 
